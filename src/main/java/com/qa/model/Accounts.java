94252af909fa126ef4d56f40ca83f2697a71c37d
MaryamRafa <maryam-rafa@hotmail.co.uk>
2018-08-28 17:36:01 +0100

    new

---
+++ src/main/java/com/qa/model/Accounts.java
@@ -0,0 +1,69 @@
+package com.qa.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Accounts {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "first_name")
+	private String firstname;
+	@Column(name = "second_name")
+	private String surname;
+	@Column(name = "account_num")
+	private String accountNum;
+	
+	public Accounts() {}
+	
+	public Accounts(Long id, String firstname, String surname, String accountNum) {
+		this.id = id;
+		this.firstname = firstname;
+		this.surname = surname;
+		this.accountNum = accountNum;
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getAccountNum() {
+		return accountNum;
+	}
+
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+	
+	
+	
+	
+
+
+}
